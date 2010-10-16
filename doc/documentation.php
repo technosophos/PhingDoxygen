05921d55e75f776cef77013388ffe92fe1187496
Matt Butcher <technosophos@gmail.com>
2010-10-15 21:34:35 -0500

    Wrote tests. Configured for testing.

--- doc/documentation.php
+++ doc/documentation.php
@@ -1,14 +1,14 @@
 <?php
-/** @mainpage @@PROJECT@@
+/** @mainpage PhingDoxygen
  * 
  * This is the main page for documentation.
  *
- * @subsection about_package About This Software
+ * @section about_package About This Software
  *
- * This is the programming reference for @@PROJECT@@. You will find complete documentation for 
+ * This is the programming reference for PhingDoxygen. You will find complete documentation for 
  * the source of this package.
  *
- * @subsection learn_more Learn More
+ * @section learn_more Learn More
  *
  * You can learn more about this package at our website.
  */
